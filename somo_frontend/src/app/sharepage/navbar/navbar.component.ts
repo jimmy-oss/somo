407e96d824d23d6d461b924d1a49534fde78fb07
jimmy-oss <jimmynjonge51@gmail.com>
2022-06-28 23:10:00 +0300

    styling the navbar

--- somo_frontend/src/app/sharepage/navbar/navbar.component.ts
+++ somo_frontend/src/app/sharepage/navbar/navbar.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
-})
+  styleUrls: ['./navbar.component.css','./animate.min.css','./aos.css','./swiper-bundle.min.css'],
+}) 
 export class NavbarComponent implements OnInit {
 
   constructor() { }
