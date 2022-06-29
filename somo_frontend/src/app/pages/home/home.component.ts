1862d43e49499acb9456f5c111f73e548b9d9044
jimmy-oss <jimmynjonge51@gmail.com>
2022-06-29 06:04:12 +0300

    styling the homepage

--- somo_frontend/src/app/pages/home/home.component.ts
+++ somo_frontend/src/app/pages/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css','./animate.min.css']
+  styleUrls: ['./home.component.css','./animate.min.css','./swiper-bundle.min.css']
 })
 export class HomeComponent implements OnInit {
 
