5ad67bf5b380bc79a5d166386b1e883dd89f5aed
“Edu58” <edumuriithi58@gmail.com>
2022-07-02 15:31:21 +0300

    adds SubmitAssignmentDetailView url path

--- somo_backend/urls.py
+++ somo_backend/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path('token-auth/', views.obtain_auth_token),
     # path('signup/', SignupView.as_view(), name='signup'),
     path('subjects/', SubjectsView.as_view(), name='subjects'),
-    path('subject/<int:pk>', SubjectsDescriptionView.as_view(), name='subject'),
+    path('subjects/<int:pk>', SubjectsDescriptionView.as_view(), name='subject'),
     path('assignments/', AssignmentsView.as_view(), name='assignments'),
-    path('assignment/<int:pk>', AssignmentsDescriptionView.as_view(), name='assignment'),
+    path('assignments/<int:pk>', AssignmentsDescriptionView.as_view(), name='assignment'),
     path('sub-assignments/', SubmitAssignmentsView.as_view(), name='submitted_assignments'),
+    path('sub-assignments/<int:pk>', SubmitAssignmentsDetailView.as_view(), name='submitted_assignment'),
 ]
