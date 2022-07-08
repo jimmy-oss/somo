ba52d02c6ed097121bf459e7d2eac4eb603b729f
“Edu58” <edumuriithi58@gmail.com>
2022-07-08 12:26:48 +0300

    adds user_login_data interface

--- somo_frontend/src/app/interfaces/interfaces.ts
+++ somo_frontend/src/app/interfaces/interfaces.ts
@@ -1,6 +1,12 @@
-export  interface new_trainer {
+export interface new_trainer {
     username: string;
     email: string;
     password1: string;
     password2: string;
+}
+
+export interface user_login_data {
+    username: string;
+    email: string;
+    password: string;
 }
