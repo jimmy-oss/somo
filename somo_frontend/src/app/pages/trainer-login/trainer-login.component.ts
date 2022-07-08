cce195936a68ad892fd6f98c2b626de548af86c1
“Edu58” <edumuriithi58@gmail.com>
2022-07-08 12:56:06 +0300

    feat: login trainer frontend

--- somo_frontend/src/app/pages/trainer-login/trainer-login.component.ts
+++ somo_frontend/src/app/pages/trainer-login/trainer-login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginServiceService } from 'src/app/Services/Auth/login-service.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-trainer-login',
@@ -7,7 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainerLoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginTrainerAPI: LoginServiceService, private router: Router) { }
+
+  LoginTrainer(trainerForm: NgForm) {
+
+    const login_data = trainerForm.value
+
+    this.loginTrainerAPI.loginTrainer(login_data).subscribe(data => {
+      if ("key" in data) {
+
+        const user_details = {
+          "username": login_data.username,
+          "key": data.key
+        }
+
+        window.localStorage.setItem('user', JSON.stringify(user_details))
+
+        this.router.navigate(['/trainer-dashboard'])
+      }
+    })
+  }
 
   ngOnInit(): void {
   }
