be2c227a234934d66617dd309784efe50c2ab73b
“Edu58” <edumuriithi58@gmail.com>
2022-07-11 11:37:48 +0300

    shows currentStudent username in dashboard & adds student logout

--- somo_frontend/src/app/pages/student-dashboard/student-dashboard.component.ts
+++ somo_frontend/src/app/pages/student-dashboard/student-dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AssignmentsServiceService } from 'src/app/Services/Assignments/assignments-service.service';
+import { LogoutServiceService } from 'src/app/Services/Auth/logout-service.service';
 import { CoursesServicesService } from 'src/app/Services/courses/courses-services.service';
+import { StudentsServicesService } from 'src/app/Services/students-data/students-services.service';
 
 @Component({
   selector: 'app-student-dashboard',
@@ -11,16 +13,27 @@ export class StudentDashboardComponent implements OnInit {
 
   all_courses: number = 0
   all_assignments: number = 0
+  current_student: any
 
-  constructor(private coursesAPI: CoursesServicesService, private assignmentsAPI: AssignmentsServiceService) { }
+  constructor(private coursesAPI: CoursesServicesService, private assignmentsAPI: AssignmentsServiceService, private studentsAPI: StudentsServicesService, private logoutAPI: LogoutServiceService) { }
 
   ngOnInit(): void {
+    this.studentsAPI.getCurrentStudentData().subscribe(data => {
+      this.current_student = data
+    })
+
     this.coursesAPI.getAllCourses().subscribe((data: any) => {
       this.all_courses = data.length
     })
 
     this.assignmentsAPI.getAllAssignments().subscribe((data: any) => {
       this.all_assignments = data.length
+    })
+  }
+
+  logout_user() {
+    this.logoutAPI.logoutTrainer().subscribe(data => {
+      console.log(data)
     })
   }
 
