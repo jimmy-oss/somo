0dae407fa98b1db414671f5128c6eff5405a9479
“Edu58” <edumuriithi58@gmail.com>
2022-07-11 11:12:38 +0300

    adds student login and register logic

--- somo_frontend/src/app/pages/registration/registration.component.ts
+++ somo_frontend/src/app/pages/registration/registration.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegisterServiceService } from 'src/app/Services/Auth/register-service.service';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private registerAPI: RegisterServiceService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  registerStudent(loginForm: NgForm) {
+    const trainer_data = loginForm.value
+
+    this.registerAPI.registerStudent(trainer_data).subscribe(data => {
+      if ("key" in data) {
+        this.router.navigate(['/login'])
+      }
+    })
   }
 
 }
